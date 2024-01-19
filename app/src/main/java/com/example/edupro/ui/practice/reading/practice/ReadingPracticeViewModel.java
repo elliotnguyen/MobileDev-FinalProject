a9a003b4a5cfac3c6d7a9c8ea25938c0590030ce
elliotnguyen <nguyentienthanhgq@gmail.com>
2024-01-19 14:49:59 +0700

    fix bug sos

--- app/src/main/java/com/example/edupro/ui/practice/reading/practice/ReadingPracticeViewModel.java
+++ app/src/main/java/com/example/edupro/ui/practice/reading/practice/ReadingPracticeViewModel.java
@@ -1,5 +1,7 @@
 package com.example.edupro.ui.practice.reading.practice;
 
+import android.util.Pair;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -16,70 +18,77 @@ import java.util.Objects;
 public class ReadingPracticeViewModel extends ViewModel {
     private final ReadingRepository readingRepository = ReadingRepository.getInstance();
     private final AnswerRepository answerRepository = AnswerRepository.getInstance();
-    private final MutableLiveData<ReadingDto> fixedReading = new MutableLiveData<>(new ReadingDto());
+    private MutableLiveData<ReadingDto> fixedReading = new MutableLiveData<>(new ReadingDto());
     private final MutableLiveData<String> readingId = new MutableLiveData<>("");
     private final MutableLiveData<Boolean> isPassageShow = new MutableLiveData<>(true);
-    //private final MutableLiveData<ReadingDto> reading = new MutableLiveData<>(new ReadingDto());
     private final MutableLiveData<Integer> numberOfQuestions = new MutableLiveData<>(0);
     private final ArrayList<MutableLiveData<String>> answers = new ArrayList<>();
     private final MutableLiveData<Integer> mark = new MutableLiveData<>(0);
+
     public void init() {
         for (int i = 0; i < 10; i++) {
-            answers.add(new MutableLiveData<>(""));
+            answers.add(new MutableLiveData<>("-"));
         }
     }
+
     public void setNumberOfQuestions(int numberOfQuestions) {
         this.numberOfQuestions.setValue(numberOfQuestions);
     }
+
     public void setAnswerAtIndex(int index, String answer) {
         answers.get(index).setValue(answer);
     }
-    public int getAnswersSelected() {
+
+    public Pair<String, String> getAnswersSelected() {
         int count = 0;
         for (MutableLiveData<String> answer : answers) {
-            if (!Objects.equals(answer.getValue(), "")) {
+            if (!Objects.equals(answer.getValue(), "-")) {
                 count++;
             }
         }
-        return count;
+        if (numberOfQuestions.getValue() == null) {
+            return new Pair<>("0", "0");
+        }
+        return new Pair<>(String.valueOf(count), String
+                .valueOf(numberOfQuestions.getValue() - count));
     }
 
     public void setReadingId(String id) {
         readingId.setValue(id);
     }
+
     public void setIsPassageShow(Boolean isPassageShow) {
         this.isPassageShow.setValue(isPassageShow);
     }
-//    public void setReading(ReadingDto reading) {
-//        this.reading.setValue(reading);
-//    }
+
     public void setFixedReading(ReadingDto reading) {
         this.fixedReading.setValue(reading);
     }
+
     public LiveData<String> getReadingId() {
         return readingId;
     }
+
     public LiveData<Boolean> getIsPassageShow() {
         return isPassageShow;
     }
+
     public LiveData<ReadingDto> getReading() {
         if (fixedReading.getValue() != null && fixedReading.getValue().getId().equals("")) {
             readingRepository.getReadingById(readingId.getValue());
-            setFixedReading(readingRepository.getReading().getValue());
-            return readingRepository.getReading();
+            return fixedReading = readingRepository.getReading();
         }
         return fixedReading;
-//        readingRepository.getReadingById(readingId.getValue());
-//        return readingRepository.getReading();
     }
-//    private final ReadingRepository readingRepository = ReadingRepository.getInstance();
 
     public LiveData<String> getAnswerAtIndex(int index) {
         return answers.get(index);
     }
+
     public LiveData<Integer> getNumberOfQuestions() {
         return numberOfQuestions;
     }
+
     private String getCurrentAnswer() {
         StringBuilder answer = new StringBuilder();
         if (numberOfQuestions.getValue() != null) {
@@ -91,6 +100,7 @@ public class ReadingPracticeViewModel extends ViewModel {
         }
         return answer.toString();
     }
+
     public LiveData<Boolean> saveAnswer(String userId, Boolean isSubmitted) {
         String currentAnswer = getCurrentAnswer();
         String id = "";
@@ -103,9 +113,11 @@ public class ReadingPracticeViewModel extends ViewModel {
         answerRepository.createAnswerByTestIdOfUserId(childId, answerDto);
         return answerRepository.getStatusHandling();
     }
+
     public LiveData<Boolean> submitAnswer(String userId) {
         return saveAnswer(userId, true);
     }
+
     public LiveData<Integer> getMark() {
         if (numberOfQuestions.getValue() != null) {
             int count = 0;
